7931f542d37f2167c19dc1ee3028137f3e6165b9
Walter Coots <walter@waltercoots.com>
2025-02-11 22:01:30 -0600

    - Added old portfolio images; we'll see if we keep them - Populated Dimensional project with old content JSON for testing - Created a new Project navigation, presently only closes the proect, but in the future will also navigate to next and previous projects - Added a loop in ProjectView.vue to display content - Renamed one thumbnail to follow the slug I'd been using

---
+++ coots-portfolio/src/components/ProjectNav.vue
@@ -0,0 +1,69 @@
+<script>
+export default {
+	name: 'ProjectNav',
+	methods: {
+		closeProject() {
+			this.$router.push('/work');
+		}
+	}
+}
+</script>
+
+<template>
+	<div class="projectNav">
+		<button class="nav-button">
+			<svg width="8" height="15" viewBox="0 0 8 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M0.5 7.49978L7.57107 0.428711V14.5708L0.5 7.49978Z" class="svg-fill"/>
+			</svg>
+		</button>
+		<button class="nav-button" @click="closeProject">
+			<svg width="22" height="21" viewBox="0 0 22 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<rect x="0.571289" width="5" height="5" class="svg-fill"/>
+				<rect x="8.57129" width="5" height="5" class="svg-fill"/>
+				<rect x="16.5713" width="5" height="5" class="svg-fill"/>
+				<rect x="0.571289" y="8" width="5" height="5" class="svg-fill"/>
+				<rect x="8.57129" y="8" width="5" height="5" class="svg-fill"/>
+				<rect x="16.5713" y="8" width="5" height="5" class="svg-fill"/>
+				<rect x="0.571289" y="16" width="5" height="5" class="svg-fill"/>
+				<rect x="8.57129" y="16" width="5" height="5" class="svg-fill"/>
+				<rect x="16.5713" y="16" width="5" height="5" class="svg-fill"/>
+			</svg>
+		</button>
+		<button class="nav-button">
+			<svg width="8" height="15" viewBox="0 0 8 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M7.64236 7.49978L0.571289 0.428711V14.5708L7.64236 7.49978Z" class="svg-fill"/>
+			</svg>
+		</button>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.projectNav {
+	text-align: center;
+	margin:0 auto 0;
+}
+.nav-button {
+	display:inline-block;
+	vertical-align: middle;
+	text-align:center;
+	width:1.5rem;
+	height:1.5rem;
+	margin:0 0.5rem;
+	background: none;
+	border: none;
+	cursor: pointer;
+	svg {
+		margin:0 auto 0;
+		.svg-fill {
+			fill: $black;
+		}
+	}
+	&:hover, &:focus {
+		svg {
+			.svg-fill {
+				fill: $accent;
+			}
+		}
+	}
+}
+</style>
--- coots-portfolio/src/views/ProjectView.vue
+++ coots-portfolio/src/views/ProjectView.vue
@@ -1,18 +1,11 @@
-<template>
-	<div class="project">
-		<h1>{{ project.title }} ({{ project.index }})</h1>
-		<span>{{ project.year }}</span>
-		<p v-html="project.description"></p>
-		<p v-html="project.credits"></p>
-		<p v-html="project.url"></p>
-		<router-link to="/work">Close</router-link>
-	</div>
-</template>
-
 <script>
 	import jsonData from '@/assets/work.json';
+	import ProjectNav from '@/components/ProjectNav.vue';
 	export default {
 		name: 'ProjectView',
+		components: {
+			ProjectNav
+		},
 		data() {
 			return {
 				project: {}
@@ -25,11 +18,74 @@
 	}
 </script>
 
-<style scoped>
+<template>
+	<div class="project">
+		<ProjectNav />
+		<div class="currentProject">
+			<h1>{{ project.title }} <span class="id">({{ project.index }})</span> <span class="year">{{ project.year }}</span></h1>
+			
+			<p v-html="project.description"></p>
+			<p v-html="project.credits"></p>
+			<p v-html="project.url"></p>
+			<div class="contents">
+				<div v-for="(item, index) in project.content" v-bind:project="item" v-bind:key="index" class="contentPiece">
+					<img v-if="item.url" :src="`/img/${project.slug}/${item.url}`" :alt="TBD" :class="item.border" />
+					<p v-if="item.caption" v-html="item.caption" class="caption"></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped lang="scss">
 .project {
+	background:$white;
 	padding: 3rem;
+	position:absolute;
+	top:0;
+	left:0;
+	width:100%;
+	height:100%;
+	overflow-y:auto;
+}
+div.currentProject {
+	div.contents {
+		margin:0 auto 0;
+		div.contentPiece {
+			margin:0 auto 4rem auto;
+			text-align:center;
+			img, video {
+				display:inline-block;
+			}
+			p.caption {
+				@include modular-scale(-3);
+				text-align:center;
+				margin:1rem auto 0 auto;
+			}
+		}
+	}
+}
+.projectNav {
+	position:sticky;
+	top:0;
+}
+p, h1 {
+	max-width:50rem;
+	margin:0 auto 0;
 }
 p {
-	max-width:40em;
+	@include modular-scale(-1);
+	margin-bottom:1rem;
 }
+h1 {
+	font-family:'Morison', serif;
+	font-weight:400;
+}
+h1 span.id {
+	display:none;
+}
+h1 span.year {
+	@include modular-scale(-3);
+}
+
 </style>
