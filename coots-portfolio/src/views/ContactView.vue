351ce08902dceb0f76bb7f919ec41b991a9e5efa
Walter Coots <walter@waltercoots.com>
2025-02-23 15:41:11 -0600

    Contact screen buttons, responsiveness

--- coots-portfolio/src/views/ContactView.vue
+++ coots-portfolio/src/views/ContactView.vue
@@ -11,12 +11,13 @@ export default {
     <div class="contactContent">
       <div class="e">
         <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#119;&#97;&#108;&#116;&#101;&#114;&#64;&#119;&#97;&#108;&#116;&#101;&#114;&#99;&#111;&#111;&#116;&#115;&#46;&#99;&#111;&#109;">&#69;&#109;&#97;&#105;&#108;</a>
-        <span>&#119;&#97;&#108;&#116;&#101;&#114;&#64;&#119;&#97;&#108;&#116;&#101;&#114;&#99;&#111;&#111;&#116;&#115;&#46;&#99;&#111;&#109;</span>
       </div>
       <div class="p">
         <a href="&#115;&#109;&#115;&#58;&#43;&#49;&#53;&#49;&#50;&#57;&#54;&#53;&#55;&#53;&#54;&#55;">&#84;&#101;&#120;&#116;</a>
         <a href="&#116;&#101;&#108;&#58;&#43;&#49;&#53;&#49;&#50;&#57;&#54;&#53;&#55;&#53;&#54;&#55;">&#67;&#97;&#108;&#108;</a>
-        <span>&#40;&#53;&#49;&#50;&#41;&#32;&#57;&#54;&#53;&#45;&#55;&#53;&#54;&#55;</span>
+      </div>
+      <div class="s">
+        <a href="https://www.linkedin.com/in/waltercoots/" target="_blank">LinkedIn</a>
       </div>
     </div>
   </div>
@@ -26,65 +27,77 @@ export default {
 div.contact {
   height:100%;
   width:100%;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  overflow:hidden;
   div.contactContent {
-    display:flex;
-    box-sizing: border-box;
-    position: absolute;
-    top:50%;
-    left:50%;
-    transform: translate(-50%, -50%);
-    div.e, div.p {
-      padding:0.5rem 0.665rem;
-      border: 1px solid $black;
-      &:last-child{
-        border-left:0;
-      }
-      span {
-        font-size:1.304375rem;
+    background:url('/public/img/contact-full.jpg') center center / cover no-repeat;
+    background-blend-mode: multiply;
+    mix-blend-mode: multiply;
+
+
+    @include xs {
+      width:22rem;
+      min-width:22rem;
+      height:17.625rem;
+    }
+    @include sm {
+      width:45.625rem;
+      min-width:45.625rem;
+      height:36.625rem;
+    }
+
+
+    position:relative;
+    div a {
+      display:block;
+      position:absolute;
+      text-indent:-999em;
+      overflow:hidden;
+      @media (hover:hover) {
+        &:hover {
+          background:$accent;
+          mix-blend-mode: screen;
+        }         
       }
     }
-    div.e a, div.p a {
-      display:inline-block;
-      position:relative;
-      color:$black;
-      text-indent:-999em;
-      width:1.5rem;
-      height:1.5rem;
-      margin-right:1rem;
-      &:before {
-        content: '';
-        display:inline-block;
-        position:absolute;
-        top:0;
-        left:0;
-        width:1.5rem;
-        height:1.5rem;
+    div.p {
+      a {
+        aspect-ratio: 1/1;
+        width:6.5%;
       }
-      &:focus {
-        &:before {
-          border-bottom:1px solid $black;
-        }
+      a:nth-child(1) {
+        top:44%;
+        left:24.5%;
+      }
+      a:nth-child(2) {
+        top:44%;
+        right:26.5%;
       }
     }
     div.e {
       a {
-        &:before {
-          background:url('~@/assets/mail.svg') no-repeat center;
-        }
+        width:61.25%;
+        height:8.2%;
+        top:52%;
+        left:19%;
       }
     }
-    div.p {
-      a:first-child {
-        &:before {
-          background:url('~@/assets/call.svg') no-repeat center;
-        }
-      }
-      a:nth-child(2) {
-        &:before {
-          background:url('~@/assets/text.svg') no-repeat center;
+    div.s {
+      a {
+        aspect-ratio: 1/1;
+        width:15%;
+        top:43.25%;
+        right:4.125%;
+        @media (hover:hover) {
+          &:hover {
+            background:#0a66c2;
+          }
         }
       }
     }
   }
 }
+
 </style>
