f9b06c227d5232325ad5ebb422ea1852ebf4b8af
Walter Coots <walter@waltercoots.com>
2025-03-12 00:57:58 -0500

    - Removed the test project because who needs it - Moved a bunch of files to misc - Added some new AFS images - Reworked logic to display portfolio content - Also reworked the JSON data for AFS

--- coots-portfolio/src/views/ProjectView.vue
+++ coots-portfolio/src/views/ProjectView.vue
@@ -61,16 +61,20 @@
 	<div class="project">
 		<ProjectNav @prev-project="prevProject" @next-project="nextProject" @close-project="closeProject" />
 		<div class="currentProject">
-			<h1>{{ project.title }} <span class="id">({{ project.index }})</span> <span class="year">{{ project.year }}</span></h1>
-			<p v-html="project.description"></p>
-			<p v-html="project.credits"></p>
-			<p v-html="project.url"></p>
+			<div class="metadata">
+				<h1>{{ project.title }}</h1>
+				<p class="year">{{ project.year }}</p>
+				<p class="role">{{ project.role }}</p>
+				<ul class="tags">
+					<li v-for="(item, index) in project.tags" v-html="item" v-bind:key="index"></li>
+				</ul>
+			</div>
 			<div class="contents">
 				<div v-for="(item, index) in project.content" v-bind:project="item" v-bind:key="index" class="contentPiece">
-					<h2 v-if="item.title" v-html="item.title" class="title"></h2>
-					<h3 v-if="item.subtitle" v-html="item.subtitle" class="title"></h3>
-					<p v-if="item.description" v-html="item.description" class="description"></p>
-					<img v-if="item.url" :src="`/img/${project.slug}/${item.url}`" :alt="project.title" :class="item.border" />
+					<h2 v-if="item.h2" v-html="item.h2"></h2>
+					<h3 v-if="item.h3" v-html="item.h3"></h3>
+					<p v-if="item.type==='paragraph'" v-html="item.copy"></p>
+					<img v-if="item.url" :src="`/img/${project.slug}/${item.url}`" :alt="project.title" :class="[item.border,item.width]" />
 					<p v-if="item.caption" v-html="item.caption" class="caption"></p>
 				</div>
 			</div>
@@ -81,7 +85,6 @@
 <style scoped lang="scss">
 .project {
 	background:$white;
-	padding: 3rem;
 	position:absolute;
 	top:0;
 	left:0;
@@ -89,15 +92,79 @@
 	height:100%;
 	overflow-y:auto;
 	z-index:3;
+	padding-top:1.5rem;
+	padding-bottom:1.5rem;
 }
 div.currentProject {
+	@include xs {
+		padding:0 1rem;
+	}
+	@include sm {
+		padding:0 1.5rem;
+	}
+	@include md {
+		padding-top:2.5rem;
+	}
+	@include xl {
+		padding-top:4rem;
+	}	
+	div.metadata {
+		margin:0 auto 1rem auto;
+		@include md {
+			max-width:50.5625rem;
+		}
+		@include lg {
+			max-width:57.56rem;
+		}
+
+		p {
+			display:inline-block;
+			@include xs {
+				@include modular-scale(-3);
+			}
+			@include xl {
+				@include modular-scale(-1);
+			}			
+		}
+		p.year {
+			&:after {
+				content:'';
+				border-right:1px solid $gray;
+				padding-left:0.5rem;
+			}
+		}
+		p.role {
+			margin-left:0.5rem;
+		}
+		ul {
+			display:block;
+			list-style:none;
+			margin-top:0.5rem;
+			@include xs {
+				@include modular-scale(-6);
+			}
+			@include xl {
+				@include modular-scale(-4);
+			}			
+			li {
+				border-radius:0.25rem;
+				background:lighten($gray,20%);
+				display:inline-block;
+				margin:0 0.5rem 0.5rem 0;
+				padding:0.25rem 0.5rem;
+			}
+		}
+	}
 	div.contents {
 		margin:0 auto 0;
 		div.contentPiece {
-			margin:0 auto 4rem auto;
+			margin:0 auto 1rem auto;
 			text-align:center;
 			img, video {
 				display:inline-block;
+				&.half {
+					max-width:20rem;
+				}
 			}
 			p.caption {
 				@include modular-scale(-3);
@@ -108,14 +175,11 @@ div.currentProject {
 	}
 }
 .projectNav {
-
 	@include xs {
 		position:fixed;
 		left:0;
 		right:0;
 		bottom:1.5rem;
-	}
-	@include sm {
 	}
 	@include md {
 		position:sticky;
@@ -123,9 +187,16 @@ div.currentProject {
 	}
 }
 p, h1, h2, h3 {
-	max-width:50rem;
+	@include md {
+		max-width:50.5625rem;
+	}
+	@include lg {
+		max-width:57.56rem;
+	}
+	orphans: 3;
 	margin:0 auto 0;
 	text-align:left;
+	line-height: calc(1ex / 0.32);
 }
 p {
 	@include modular-scale(-1);
@@ -135,12 +206,29 @@ h1, h2, h3 {
 	font-family:'Morison', serif;
 	font-weight:400;
 }
-h1 span.id {
-	display:none;
+h1 {
+	margin-bottom:1rem;
+	@include xs {
+		@include modular-scale(3);
+	}
+	@include xl {
+		@include modular-scale(5);
+	}
 }
-h1 span.year {
-	@include modular-scale(-3);
+h2 {
+	@include xs {
+		@include modular-scale(3);
+	}
+	@include xl {
+		@include modular-scale(5);
+	}	
 }
-
-
+p {
+	@include xs {
+		@include modular-scale(-2);
+	}
+	@include xl {
+		@include modular-scale(0);
+	}	
+}
 </style>
