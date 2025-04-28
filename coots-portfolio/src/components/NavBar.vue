7d9efc4b3c9dcff107d51f8b97f464c7300d2bdf
Walter Coots <walter@waltercoots.com>
2025-04-28 16:26:02 -0500

    Minor styling updates to Contact menu

--- coots-portfolio/src/components/NavBar.vue
+++ coots-portfolio/src/components/NavBar.vue
@@ -5,13 +5,18 @@
 		<router-link to="/about">About</router-link>
 		<a href="/resume.pdf" download="walter-coots-résumé">Résumé</a>
 		<!-- <a href="https://blog.waltercoots.com/" target="_blank">Blog</a> -->
-		<div class="contactMenu" :class="{ open: contactOpen }" @click="contactOpen = !contactOpen">
-			Contact
+		<div class="contactMenu" :class="{ open: contactOpen }" @click="contactOpen = !contactOpen"
+			@mouseleave="closeMenu" ref="menuRef">
+			<span>Contact</span> 
+			<svg width="9" height="5" viewBox="0 0 9 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M1.91406 0.569824H7.08594C7.50343 0.569938 7.72549 1.04315 7.49219 1.36182L7.43945 1.42334L4.85352 4.00928C4.65827 4.20444 4.34173 4.20444 4.14648 4.00928L1.56055 1.42334C1.24571 1.10838 1.46875 0.569946 1.91406 0.569824Z" class="norgie"/>
+			</svg>
+			
 			<div v-if="contactOpen" class="submenu">
-				<a href="mailto:walter@waltercoots.com">Email</a>
-				<a href="sms:5129657567">Text</a>
-				<a href="tel:5129657567">Call</a>
-				<a href="http://linkedin.com/in/waltercoots/" target="_blank">In</a>
+				<a href="mailto:walter@waltercoots.com" @click="closeMenu">Email</a>
+				<a href="sms:5129657567" @click="closeMenu">Text</a>
+				<a href="tel:5129657567" @click="closeMenu">Call</a>
+				<a href="http://linkedin.com/in/waltercoots/" target="_blank" @click="closeMenu">LinkedIn</a>
 			</div>
 		</div>
 	</nav>
@@ -44,39 +49,73 @@
 			&:hover {
 				text-decoration: none;
 				color: $accent;
+				.norgie {
+					stroke: $accent;
+					transform: rotate(0deg);
+				}
 			}
 		}
-		&::after {
-			content: '';
+		span {
 			display: inline-block;
-			width: 0.625rem;
-			height: 0.625rem;
-			background: url('@/assets/norgie.svg') no-repeat center;
-			background-size: contain;
-			transform: rotate(0deg);
-			transition: transform 0.13s ease;
 		}
-		&.open::after {
+		svg {
+			width:0.625rem;
+			height:0.625rem;
+			margin-left:0.25rem;
+			display: inline-block;
+			transition: transform 0.13s ease;
+			transform: rotate(0deg);
+		}
+		.norgie {
+			stroke:$black;
+		}
+		
+		&.open svg {
 			transform: rotate(180deg);
 		}
 		.submenu {
 			display: flex;
-			flex-direction: row;
-			text-align:right;
+			flex-direction: column;
+			text-align:left;
 			position: absolute;
+			width:100%;
 			font-size:0.8em;
 			top: 100%;
-			right:0;
+			padding:0.125rem 0.5rem 1.5rem 0.5rem;
+			margin-left:0.5rem;
+			box-sizing: border-box;
+			left:0;
 			background: $white;
 			a {
 				color: $black;
 				text-decoration: none;
+				padding:0.125rem 0;
 			}
 		}
 	}
 </style>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+
 const contactOpen = ref(false);
+const menuRef = ref(null);
+
+function handleDocumentClick(event) {
+  if (menuRef.value && !menuRef.value.contains(event.target)) {
+    contactOpen.value = false;
+  }
+}
+
+onMounted(() => {
+  document.addEventListener('click', handleDocumentClick);
+});
+
+onBeforeUnmount(() => {
+  document.removeEventListener('click', handleDocumentClick);
+});
+
+function closeMenu() {
+  contactOpen.value = false;
+}
 </script>
