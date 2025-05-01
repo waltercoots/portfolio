5fb06195c85411d4707ca6e68e792871c8cc7408
Walter Coots <walter@waltercoots.com>
2025-04-30 23:55:43 -0500

    Better Home icon

--- coots-portfolio/src/components/NavBar.vue
+++ coots-portfolio/src/components/NavBar.vue
@@ -1,6 +1,11 @@
 <template>
 	<nav class="mainNav">
-		<router-link to="/" class="home" title="Home">&#8962;</router-link>
+		<router-link to="/" class="home" title="Home">
+			<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M4 8V12C4 12.5523 4.44772 13 5 13H11C11.5523 13 12 12.5523 12 12V8" stroke-linecap="round" class="svg-line" />
+			<path d="M14 7L8.70711 1.70711C8.31658 1.31658 7.68342 1.31658 7.29289 1.70711L1.95402 7.04598" class="svg-line" stroke-linecap="round" stroke-linejoin="round" />
+			</svg>
+		</router-link>
 		<router-link to="/about" title="About">About</router-link>
 		<router-link to="/work" title="Work">Work</router-link>
 		<!-- <a href="https://blog.waltercoots.com/" target="_blank">Blog</a> -->
@@ -8,7 +13,7 @@
 			@mouseleave="closeMenu" ref="menuRef">
 			Contact
 			<svg width="9" height="5" viewBox="0 0 9 5" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path d="M1.91406 0.569824H7.08594C7.50343 0.569938 7.72549 1.04315 7.49219 1.36182L7.43945 1.42334L4.85352 4.00928C4.65827 4.20444 4.34173 4.20444 4.14648 4.00928L1.56055 1.42334C1.24571 1.10838 1.46875 0.569946 1.91406 0.569824Z" class="norgie"/>
+			<path d="M1.91406 0.569824H7.08594C7.50343 0.569938 7.72549 1.04315 7.49219 1.36182L7.43945 1.42334L4.85352 4.00928C4.65827 4.20444 4.34173 4.20444 4.14648 4.00928L1.56055 1.42334C1.24571 1.10838 1.46875 0.569946 1.91406 0.569824Z" class="svg-line" />
 			</svg>
 			<div v-if="contactOpen" class="submenu" title="Contact...">
 				<a href="mailto:walter@waltercoots.com" @click="closeMenu" title="Email">Email</a>
@@ -25,6 +30,9 @@
 	nav.mainNav {
 		display:flex;
 		justify-content: center;
+		.svg-line {
+			stroke:$black;
+		}
 		a {
 			padding:0 0.5em;
 			color:$black;
@@ -35,6 +43,15 @@
 				&:hover {
 					text-decoration: none;
 					color: $accent;
+
+					.svg-line {
+						stroke:$accent;
+					}
+				}
+			}
+			&.home {
+				svg {
+					margin-top:0.5rem;
 				}
 			}
 		}
@@ -48,7 +65,7 @@
 			&:hover {
 				text-decoration: none;
 				color: $accent;
-				.norgie {
+				.svg-line {
 					stroke: $accent;
 					transform: rotate(0deg);
 				}
@@ -65,10 +82,6 @@
 			transition: transform 0.13s ease;
 			transform: rotate(0deg);
 		}
-		.norgie {
-			stroke:$black;
-		}
-		
 		&.open svg {
 			transform: rotate(180deg);
 		}
